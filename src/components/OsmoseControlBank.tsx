b740884ee0e0590b5cd0a753ad9bd27bf83bd358
Rune Ejnar Bang Lejbølle <rune.e.b.lejboelle@gmail.com>
2026-08-19 00:22:57 +0200

    Improve mixer fader controls

--- src/components/OsmoseControlBank.tsx
+++ src/components/OsmoseControlBank.tsx
@@ -13,10 +13,13 @@ export function OsmoseControlBank(props: OsmoseControlBankProps) {
       <div class="fader-grid">
         <For each={props.parameters}>{(parameter) => {
           const id = `cc-${parameter.controlChange}`
+          const value = () => props.values[parameter.controlChange] ?? 0
           return <label classList={{ fader: true, [`fader-${parameter.group}`]: true }} for={id}>
-            <span class="fader-value">{props.values[parameter.controlChange] ?? 0}</span>
-            <span class="fader-track">
-              <input id={id} type="range" min="0" max="127" value={props.values[parameter.controlChange] ?? 0}
+            <span class="fader-value">{value()}</span>
+            <span class="fader-track" style={{ '--fader-position': `${(value() / 127) * 100}%` }}>
+              <span class="fader-ticks" aria-hidden="true"><i /><i /><i /><i /><i /></span>
+              <span class="fader-cap" aria-hidden="true"><i /></span>
+              <input id={id} type="range" min="0" max="127" value={value()}
                 aria-label={`${parameter.name}, CC ${parameter.controlChange}`}
                 onInput={(event) => props.onValueChange(parameter.controlChange, Number(event.currentTarget.value))} />
             </span>
