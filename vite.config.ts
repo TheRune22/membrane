90989accd3b39a673a9e991dbd3e3385acdc9b3d
Rune Ejnar Bang Lejbølle <rune.e.b.lejboelle@gmail.com>
2026-08-24 22:27:06 +0200

    Revert "Add Cloudflare Quick Tunnel server configuration"
    This reverts commit 0e7255722cff28d7969bc8d25abd9cef694dd512.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
-  // For testing with Cloudflare Quick Tunnel
-  server: {
-    allowedHosts: ['.trycloudflare.com'],
-  },
   test: {
     environment: 'node',
   },
